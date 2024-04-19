c2e12efb7eed25a6573b2a7d198d09fb82b5b453
obata-nstg <obata@nstg.co.jp>
2024-04-19 10:44:46 +0900

    docker react環境構築、laravelとreact連携テスト

--- src/config/cors.php
+++ src/config/cors.php
@@ -1,7 +1,6 @@
 <?php
 
 return [
-
     /*
     |--------------------------------------------------------------------------
     | Cross-Origin Resource Sharing (CORS) Configuration
@@ -19,16 +18,15 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['http://localhost:3000'],
 
     'allowed_origins_patterns' => [],
 
-    'allowed_headers' => ['*'],
+    'allowed_headers' => ['Content-Type', 'X-Requested-With'],
 
     'exposed_headers' => [],
 
     'max_age' => 0,
 
     'supports_credentials' => false,
-
 ];
